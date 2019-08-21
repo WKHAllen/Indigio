4f3a4a483552ceb65fa14d86761457aa4223be06
Will Allen <wkhallen@gmail.com>
2019-08-21 09:08:18 -0400

    Fix about page electron links

--- src/server/public/about/about.js
+++ src/server/public/about/about.js
@@ -1,13 +1,6 @@
 window.addEventListener('load', () => {
     if (isElectron) {
-        const open = require('open');
         var repoLink = document.getElementById('repo-link');
-        var repoURL = repoLink.getAttribute('href');
-        repoLink.onclick = async function() {
-            await open(repoURL);
-        }
-        repoLink.removeAttribute('href');
-        repoLink.removeAttribute('target');
-        repoLink.style.cursor = 'pointer';
+        electronifyLink(repoLink);
     }
 });
