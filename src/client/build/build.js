7eb6972cdb22ae30ceae6511edbc8e911268dbb6
WKHAllen <wkhallen@gmail.com>
2019-08-27 13:20:42 -0400

    Improve electron app building/packaging

--- src/client/build/build.js
+++ src/client/build/build.js
@@ -1,19 +1,20 @@
 const packager = require('electron-packager');
-const electronInstaller = require('electron-winstaller');
+const path = require('path');
 const fs = require('fs');
 
 var optionsFile = '../package.json';
 var appName = 'Indigio';
 
-async function build(platform, arch, options) {
+async function packageApp(platform, options) {
     var iconFile = '../assets/favicon.ico';
-    if (platform === 'darwin') {
+    if (platform === 'darwin')
         iconFile = '../assets/favicon.icns';
-    }
+    if (platform === 'linux')
+        iconFile = 'resources/app/assets/favicon.png';
     var bundleOptions = {
         'dir': '..',
         'platform': platform,
-        'arch': arch,
+        'arch': 'all',
         'appVersion': options.version,
         'asar': false,
         'executableName': appName,
@@ -23,11 +24,22 @@ async function build(platform, arch, options) {
         'out': 'build',
         'overwrite': true
     };
-    await packager(bundleOptions);
-    if (platform === 'win32') {
-        var winstallerOptions = {
-            'appDirectory': `build/${appName}-${platform}-${arch}`,
-            'outputDirectory': `dist/${appName}-${platform}-${arch}`,
+    return await packager(bundleOptions);
+}
+
+async function packageAll(options) {
+    await packageApp('all', options);
+}
+
+async function buildWindows(options) {
+    const windowsInstaller = require('electron-winstaller');
+    var packagePaths = await packageApp('win32', options);
+    var iconFile = '../assets/favicon.ico';
+    for (var packagePath of packagePaths) {
+        var packageDir = path.basename(packagePath);
+        var windowsOptions = {
+            'appDirectory': `build/${packageDir}`,
+            'outputDirectory': `dist/${packageDir}`,
             'authors': 'Will Allen',
             'exe': `${appName}.exe`,
             'description': options.description,
@@ -39,28 +51,77 @@ async function build(platform, arch, options) {
             'setupExe': `${appName}-${options.version}-setup.exe`,
             'noMsi': true
         };
-        await electronInstaller.createWindowsInstaller(winstallerOptions);
-    } else if (platform === 'darwin') {
-        // TODO: create macOS installer
-    } else if (platform === 'linux') {
-        // TODO: create linux installer
+        await windowsInstaller.createWindowsInstaller(windowsOptions);
     }
+}
+
+async function buildDarwin(options) {
+    const darwinInstaller = require('electron-installer-dmg');
+    var packagePaths = await packageApp('darwin', options);
+    var iconFile = '../assets/favicon.icns';
+    for (var packagePath of packagePaths) {
+        var packageDir = path.basename(packagePath);
+        var darwinOptions = {
+            'appPath': `build/${packageDir}/${appName}.app`,
+            'name': appName,
+            'title': appName,
+            'icon': iconFile,
+            'overwrite': true,
+            'debug': true,
+            'out': `dist/${packageDir}`
+        };
+        darwinInstaller(darwinOptions, (err) => {
+            if (err) throw err;
+        });
+    }
+}
+
+async function buildLinux(options) {
+    const linuxInstaller = require('electron-installer-debian');
+    var packagePaths = await packageApp('linux', options);
+    var iconFile = 'resources/app/assets/favicon.png';
+    for (var packagePath of packagePaths) {
+        var packageDir = path.basename(packagePath);
+        var linuxOptions = {
+            'src': `build/${packageDir}`,
+            'dest': `dist/${packageDir}`,
+            'name': appName.toLowerCase(),
+            'productName': appName,
+            'genericName': appName,
+            'description': options.description,
+            'productDescription': options.description,
+            'version': options.version,
+            'maintainer': options.author,
+            'homepage': 'https://github.com/WKHAllen/Indigio',
+            'bin': appName,
+            'icon': iconFile
+        };
+        await linuxInstaller(linuxOptions);
+    }
+}
+
+async function buildPrimary(options) {
+    await buildWindows(options);
+    await buildDarwin(options);
+    await buildLinux(options);
+}
+
+async function buildCurrent(options) {
+    if (process.platform === 'win32')
+        await buildWindows(options);
+    else if (process.platform === 'darwin')
+        await buildDarwin(options);
+    else if (process.platform === 'linux')
+        await buildLinux(options);
+    else
+        throw 'platform not supported';
 }
 
 async function main() {
     fs.readFile(optionsFile, 'utf8', async (err, data) => {
         if (err) throw err;
         var options = JSON.parse(data);
-        var builds = [
-            ['win32', 'ia32'],
-            // ['win32', 'x64'],
-            // ['darwin', 'x64'], // (node:14820) UnhandledPromiseRejectionWarning: TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be one of type string, Buffer, or URL. Received type undefined
-            ['linux', 'ia32'],
-            // ['linux', 'x64']
-        ];
-        for (var b of builds) {
-            await build(b[0], b[1], options);
-        }
+        await buildCurrent(options);
     });
 }
 
